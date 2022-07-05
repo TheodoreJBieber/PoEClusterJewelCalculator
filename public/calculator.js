f2ab941457d8f07cf9ecb48bc3fc61a34aa46b9c
TheodoreJBieber <tbaslp@yahoo.com>
2022-07-05 12:30:06 -0400

    Data is hooked up, and 3n2d is working. Needs some convenience features - such as mod descriptions, trade link generation, search boxes, narrowing down mods by bases, suffix, pools. Need to improve output to sructure based on bases

--- public/calculator.js
+++ public/calculator.js
@@ -1,10 +1,36 @@
 
-function calculate3n2d(event) {
-	var output = getOutput("output3n2d");
-	writeToOutput(output, "testText");
+function calculate3n2d(event, inid1, inid3) {
+	var in1 = getElement(inid1).value;
+	var in3 = getElement(inid3).value;
+	console.log(in1);
+	console.log(in3);
+	var text = "";
+	var between = [];
+	for (const num in sortOrderReverseMap) {
+		if (isBetween(in1, in3, num)) {
+			console.log(num);
+			between.push(num);
+			text = text + sortOrderReverseMap[num] + ", ";
+		}
+	} 
+
+	console.log(text);
+
+	var output = getElement("output3n2d");
+	writeToOutput(output, text);
 }
 
-function getOutput(id) {
+
+// returns true if between is between val1 and val3. there cannot be overlap in notables, so no equals comparison
+function isBetween(val1, val3, between) {
+	if (val1 < val3) {
+		return (val1 < between) && (val3 > between);
+	} else {
+		return (val3 < between) && (val1 > between);
+	}
+}
+
+function getElement(id) {
 	return document.getElementById(id);
 }
 
