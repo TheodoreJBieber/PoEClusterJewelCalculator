52094f54ef0e8764b5bd7b99848880b72463c488
TheodoreJBieber <tbaslp@yahoo.com>
2023-08-27 14:22:15 -0400

    add +

--- src/components/TradeTemplates.js
+++ src/components/TradeTemplates.js
@@ -9,7 +9,7 @@ class TradeTemplates extends React.Component {
             <TemplateUrl tradePathBase={this.props.tradePathBase} min={2} max={3} description="Any Small (2-3 Passives)"></TemplateUrl>
             <TemplateUrl tradePathBase={this.props.tradePathBase} min={4} max={5} description="Any Medium (4-5 Passives)"></TemplateUrl>
             <TemplateUrl tradePathBase={this.props.tradePathBase} min={8} max={8} description="Any Large (8 Passives)"></TemplateUrl>
-            <TemplateUrl tradePathBase={this.props.tradePathBase} min={12} max={12} ilvlMin={84} description="Any iLvl 84 Large 12 Passive Cluster Jewel"></TemplateUrl>
+            <TemplateUrl tradePathBase={this.props.tradePathBase} min={12} max={12} ilvlMin={84} description="Any iLvl 84+ Large 12 Passive Cluster Jewel"></TemplateUrl>
             </div>
         )
     }
